5a682b0a31368bef70d5e1c093a05f98ecbd10b6
Arseniy Bondar <arsenbond2001@gmail.com>
2024-04-09 19:53:43 +0300

    Update server.ts

--- app/config/server.ts
+++ app/config/server.ts
@@ -104,7 +104,7 @@ export const getServerSideConfig = () => {
 
     gtmId: process.env.GTM_ID,
 
-    needCode: ACCESS_CODES.size > 100,
+    needCode: ACCESS_CODES.size > 0,
     code: process.env.CODE,
     codes: ACCESS_CODES,
 
